b554415bffacc77548896a68a16c852e4a7b4713
DeanGadberry <dean@deangadberry.com>
2022-06-17 10:52:37 -0500

    changed sensitive information

--- cosc1436/ch2/217.cpp
+++ cosc1436/ch2/217.cpp
@@ -10,7 +10,7 @@ int main()
     string name, address, phoneNumber;
     name = "Dean Russell Gadberry";
     address = "2 BlackJack Lane, Copper Canyon, TX 75022";
-    phoneNumber = "(562) 619-6025";
+    phoneNumber = "(555) 555-6025";
 
     cout << name << endl << address << endl << phoneNumber << endl;
     return 0;
